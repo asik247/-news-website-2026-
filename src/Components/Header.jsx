095f5fedc09f6bc8d966017bc0543f71eb8eb85e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-29 22:59:32 +0600

    Header in date fns use

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import logoImage from "../assets/logo.png"
+import { format } from 'date-fns';
 const Header = () => {
     return (
-        <div>
-            <img src={logoImage} alt="" />
+        <div className='flex flex-col justify-center items-center gap-2'>
+            <img className='w-[400px]' src={logoImage} alt="" />
+            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            {/* Date fns use code here */}
+            <p>{format(new Date(),'EEEE,MMMM,d,yyy')}</p>
         </div>
     );
 };
