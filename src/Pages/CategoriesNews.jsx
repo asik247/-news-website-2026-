5949444bf0d363f59552e0ba4586743b7588f010
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-30 16:29:45 +0600

    dynamicaly load news

--- src/Pages/CategoriesNews.jsx
+++ src/Pages/CategoriesNews.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoriesNews = () => {
+    const { id } = useParams();
+    // console.log(id);
+    const allNews = useLoaderData();
+    // console.log(allNews);
+    const [news, setNews] = useState([]);
+    useEffect(() => {
+        if (id == '0') {
+            setNews(allNews);
+            return;
+        } else if (id == '1') {
+            const filterNews = allNews.filter(news => news.others.is_today_pick == true);
+            setNews(filterNews);
+        } else {
+            const filterNews = allNews.filter(news => news.category_id == id);
+            // console.log(filterNews);
+            setNews(filterNews)
+        }
+
+    }, [allNews, id])
     return (
         <div>
-            <h2>All News here</h2>
+            <h2>All News here ({news.length})</h2>
         </div>
     );
 };
--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -1,7 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
+import Home from "../Components/Home";
+import CategoriesNews from "../Pages/CategoriesNews";
 
 const router = createBrowserRouter([
-    {path:'/',Component:Root}
+    {path:'/',Component:Root,children:[
+        {index:true,Component:Home},
+        {
+            path:'categoriesNews/:id',
+            loader:()=>fetch("/news.json"),
+            Component:CategoriesNews
+        }
+    ]}
 ])
 export default router;
