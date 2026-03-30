481f056ac0b0253aa867c62beff965dcef06f90d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-30 07:07:56 +0600

    google font added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,9 +5,10 @@ const Header = () => {
     return (
         <div className='flex flex-col justify-center items-center gap-2'>
             <img className='w-[400px]' src={logoImage} alt="" />
-            <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='text-gray-500'>Journalism Without Fear or Favour</p>
             {/* Date fns use code here */}
-            <p>{format(new Date(),'EEEE,MMMM,d,yyy')}</p>
+            <p>{format(new Date(), 'EEEE,MMMM,d,yyy')}</p>
+
         </div>
     );
 };
