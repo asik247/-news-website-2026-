0135f8dfc60da0700c860e2cadb0ac8c21f9d591
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-03 00:26:05 +0600

    firebser file create

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCxfTRji_Hj3rV3loPmEml99TebqLgwJgU",
+  authDomain: "news-website-2026.firebaseapp.com",
+  projectId: "news-website-2026",
+  storageBucket: "news-website-2026.firebasestorage.app",
+  messagingSenderId: "574735814788",
+  appId: "1:574735814788:web:9d05822033858564ccb2d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
