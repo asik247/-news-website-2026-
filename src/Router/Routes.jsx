96b0fd5d174073c2a099d89e486360909c004725
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-02 10:02:01 +0600

    login,registation form design

--- src/Router/Routes.jsx
+++ src/Router/Routes.jsx
@@ -3,18 +3,28 @@ import Root from "../Root/Root";
 import Home from "../Components/Home";
 import CategoriesNews from "../Pages/CategoriesNews";
 import Details from "../Components/Details";
+import AuthLayout from "../Layouts/AuthLayout";
 
 const router = createBrowserRouter([
-    {path:'/',Component:Root,children:[
-        {index:true,Component:Home},
-        {
-            path:'categoriesNews/:id',
-            loader:()=>fetch("/news.json"),
-            Component:CategoriesNews
-        },
-        {path:'details/:id',
-         loader:({params})=>fetch("/news.json").then(res=>res.json()).then(data=>data.find(n=>n.id == params.id)),
-        Component:Details}
-    ]}
+    {
+        path: '/', Component: Root, children: [
+            { index: true, Component: Home },
+            {
+                path: 'categoriesNews/:id',
+                loader: () => fetch("/news.json"),
+                Component: CategoriesNews
+            },
+            {
+                path: 'details/:id',
+                loader: ({ params }) => fetch("/news.json").then(res => res.json()).then(data => data.find(n => n.id == params.id)),
+                Component: Details
+            },
+
+        ]
+    },
+      {
+        path: 'auth',
+        Component: AuthLayout
+    }
 ])
 export default router;
