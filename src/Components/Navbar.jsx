5776d6a0b92bf00bcc042478c5b1294d19bff025
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-03 18:09:06 +0600

    logOut navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, NavLink } from 'react-router';
 import userImages from "../assets/user.png"
+import { AuthContext } from '../Context/AuthContext';
 const Navbar = () => {
+    const { user, logOut } = useContext(AuthContext);
+    // console.log(user);
+    const handleLogOut = () => {
+        logOut()
+            .then(() => {
+                alert('LogOut done')
+            }).catch(error => {
+                console.log(error.message);
+            })
+    }
     return (
         <div className='flex justify-between items-center w-11/12 mx-auto mt-10'>
+            <div>
+                {user && <p>{user.email}</p>}
+            </div>
             <div className=''></div>
             <div className='nav flex gap-4 text-gray-400'>
                 <NavLink to={'/'}>Home</NavLink>
                 <NavLink to={'/about'}>About</NavLink>
                 <NavLink to={'/career'}>Career</NavLink>
-               
+
             </div>
             <div className="login flex items-center gap-5">
                 <img src={userImages} alt="" />
-                <Link to={'/auth/login'} className='btn btn-primary'>LogIn</Link>
+                {user ?( <button onClick={handleLogOut} className='btn btn-primary'>LogOut</button>) : (<Link to={'/auth/login'} className='btn btn-primary'>LogIn</Link>)}
+
             </div>
-     
+
 
         </div>
     );
